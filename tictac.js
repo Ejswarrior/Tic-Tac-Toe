c318c3a2c683396b8890720faac37cabfaefc037
Erik Sabin <eriksabin97@gmail.com>
2022-08-16 22:25:52 -0400

    adding to github so I can work on my laptop

---
+++ tictac.js
@@ -0,0 +1,6 @@
+//We have 3 rows and each row has a box. We can allow the player to click on a box and update the textContent
+//Once we have a row = 3. For that when that row is click we can add +1 to the row and if row = 3 then win
+// variable to switch players
+// Play one is a x and player two is a circle 
+
+let switchPlayer = 1;
