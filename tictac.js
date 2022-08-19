f5c8a5679bcda7de1a37cdba1ec7547e2de812da
Erik Sabin <eriksabin97@gmail.com>
2022-08-18 23:42:56 -0400

    game is complete with a reset button

--- tictac.js
+++ tictac.js
@@ -22,27 +22,81 @@ let boix6 =  document.querySelector('.box6')
 let boix7 =  document.querySelector('.box7')
 let boix8 =  document.querySelector('.box8')
 let boix9 =  document.querySelector('.box9')
+win = 0;
 console.log('yo')
+
+function winCondition(con, player){
+   
+if(boix1.textContent == con && boix2.textContent == con && boix3.textContent == con){
+    document.querySelector('.hero').textContent = player + ", You win!"
+    win += 1
+} else if(boix4.textContent == con && boix5.textContent == con && boix6.textContent == con){
+    document.querySelector('.hero').textContent = player + ", You win!"
+    win += 1
+} else if(boix7.textContent == con && boix8.textContent == con && boix9.textContent == con){
+    document.querySelector('.hero').textContent = player + ", You win!"
+    win += 1
+}else if(boix1.textContent == con && boix4.textContent == con && boix7.textContent == con){
+    document.querySelector('.hero').textContent = player + ", You win!"
+    win += 1
+}else if(boix2.textContent == con && boix5.textContent == con && boix8.textContent == con){
+    document.querySelector('.hero').textContent = player + ", You win!"
+    win += 1
+}else if(boix3.textContent == con && boix6.textContent == con && boix9.textContent == con){
+    document.querySelector('.hero').textContent = player + ", You win!"
+    win += 1
+}else if(boix1.textContent == con && boix5.textContent == con && boix9.textContent == con){
+    document.querySelector('.hero').textContent = player + ", You win!"
+    win += 1
+}else if(boix3.textContent == con && boix5.textContent == con && boix7.textContent == con){
+    document.querySelector('.hero').textContent = player + ", You win!"
+    win += 1
+}
+
+}
 
 function boxClick(name, selector, turn){
 document.querySelector(name).addEventListener('click', function(){
+if (win == 0){
+if(switchPlayer == 1){
+selector.textContent = 'X'
 
-if(switchPlayer == 1 && turn == 0){
-selector.insertAdjacentHTML("beforeend",'<div>X</div>')
 switchPlayer += 1;
-document.querySelector('h2').textContent = 'Player 2 Turn'
+document.querySelector('h2').textContent = 'Player 2\'s Turn'
+winCondition('X', 'Player 1')
 
-} else if(switchPlayer == 2 && turn == 0){
-    selector.insertAdjacentHTML("beforeend",'<div>O</div>') 
+
+} else if(switchPlayer == 2){
+    selector.textContent = 'O'
     switchPlayer -= 1;
-    document.querySelector('h2').textContent = 'Player 1 Turn'
+    document.querySelector('h2').textContent = 'Player 1\'s Turn'
+    winCondition('O', 'Player 2')
 }
 
 console.log(switchPlayer)
 
 turn += 1;
+}
 })
 }
+document.querySelector('.btn').addEventListener('click', function(){
+
+boix1.textContent = ''
+boix2.textContent = ''
+boix3.textContent = ''
+boix4.textContent = ''
+boix5.textContent = ''
+boix6.textContent = ''    
+boix7.textContent = ''
+boix8.textContent = ''
+boix9.textContent = ''
+win = 0
+switchPlayer = 1
+document.querySelector('h2').textContent = 'Player 1 Start';
+document.querySelector('.hero').textContent = ""
+turn = 0;
+})
+
 
 boxClick('.box1', boix1, turn1)
 boxClick('.box2', boix2, turn2)
